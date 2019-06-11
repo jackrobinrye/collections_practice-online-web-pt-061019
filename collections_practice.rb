68a80efb75a1c4e8fabce7e8085848cef7ba65c1
Jack Atherton <jackrrye@gmail.com>
2019-06-11 20:07:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+require "pry"
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  i = 0 
+  ordered_array = array.sort
+  return_array = []
+  while i < ordered_array.length do 
+    return_array << ordered_array.pop
+  end
+  return_array
+end
+
+def sort_array_char_count(array)
+  array.sort_by do |word| 
+    word.length
+  end
+end
+
+def swap_elements
+  
+end
